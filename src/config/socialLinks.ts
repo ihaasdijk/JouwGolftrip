31c9813e07cbc6ddd6e5c6dedad4f7036cc594fe
ingmar-rituals <ingmar.haasdijk@rituals.com>
2025-07-07 15:51:58 +0200

    instagram logo

--- src/config/socialLinks.ts
+++ src/config/socialLinks.ts
@@ -10,18 +10,8 @@ export interface SocialLink {
 
 export const socialLinks: SocialLink[] = [
 	{
-		name: 'facebook',
+		name: 'instagram',
 		link: '/',
-		icon: 'fb-icon'
-	},
-	{
-		name: 'twitter',
-		link: '/',
-		icon: 'twitter-icon'
-	},
-	{
-		name: 'discord',
-		link: '/',
-		icon: 'discord-icon'
+		icon: 'insta-icon'
 	}
 ]
